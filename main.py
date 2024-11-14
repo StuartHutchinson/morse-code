243fc0676bfd78158d2af390512109b42aaa192e
StuartHutchinson <33181089+StuartHutchinson@users.noreply.github.com>
2024-11-14 10:33:29 +0000

    convert to basic webapp

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import os
 
+from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+bootstrap = Bootstrap5(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	form = BasicForm()
+	encoded = ""
+	if form.validate_on_submit():
+		encoded = encode(form.user_input.data)
+	return render_template("index.html", form=form, encoded=encoded)
+	# return "Morse Code!"
+	# return "Test Test      Test"
+	# return encode()
 
 code = {'A':'▄ ▄▄▄',
         'B':'▄▄▄ ▄ ▄ ▄',
@@ -42,9 +64,17 @@ code = {'A':'▄ ▄▄▄',
         "'":'▄ ▄▄▄ ▄▄▄ ▄▄▄ ▄▄▄ ▄',
         " ":'    '} #word space is 7 - we will add 3 more below so just add 4 here
 
-to_convert = input("Enter a phrase to encode:")
-converted = ""
-for char in to_convert:
-    converted += code[char.upper()] + '   ' #3 spaces to denote a letter space
-print("In Morse code:")
-print(converted)
+class BasicForm(FlaskForm):
+	user_input = StringField("Text to encode", validators=[DataRequired()])
+	submit = SubmitField("Encode")
+
+def encode(to_encode):
+	# to_encode = input("Enter a phrase to encode:")
+	converted = ""
+	for char in to_encode:
+		converted += code[char.upper()] + '   ' #3 spaces to denote a letter space
+	print(converted)
+	return converted
+
+if __name__ == "__main__":
+	app.run(debug=True)
